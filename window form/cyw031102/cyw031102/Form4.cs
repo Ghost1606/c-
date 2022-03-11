aa12cc854a386669d268c0d969edb5cd4f0b6535
Ghost1606 <101306645+Ghost1606@users.noreply.github.com>
2022-03-11 16:04:50 +0900

    Add files via upload

---
+++ window form/cyw031102/cyw031102/Form4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cyw031102
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double t1 = 0;
+            double t2 = 0;
+            double t3 = 0;
+
+            t1 = double.Parse(textBox1.Text);
+            t2 = double.Parse(textBox2.Text);
+
+            t3 = t1 * t2;
+
+            if (t1 >= 10)
+            {
+                if (t1 >= 20)
+                {
+                    
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                textBox3.Text = t3.ToString();
+            }
+        }
+    }
+}
